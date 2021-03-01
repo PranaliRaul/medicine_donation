293c57bba607ebb18faf53f947066b5dccc850ad
Narendra Gawade <Narendra.Gawade@extentia.com>
2021-03-01 21:53:41 +0530

    navigation ka code likha hai baki module mai kar lo

--- src/app/share/navigation/navigation.component.ts
+++ src/app/share/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { RegisterService } from 'src/app/register/register.service';
 
 @Component({
   selector: 'app-navigation',
@@ -6,12 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
-
-
-  
-  constructor() { }
+public navigation = []
+public userdata = []
+  constructor(private service:RegisterService) { }
 
   ngOnInit() {
+    this.userdata = JSON.parse(localStorage.getItem('userdata'));
+    if(!this.userdata){
+      this.navigation = [];
+    } else if(this.userdata[0].roleId === 1){
+      this.navigation = environment.ADMIN_NAVIGATION
+    } else if(this.userdata[0].roleId === 2){
+      this.navigation = environment.NGO_NAVIGATION
+    }else if(this.userdata[0].roleId === 3){
+      this.navigation = environment.ADMIN_NAVIGATION
+    }else if(this.userdata[0].roleId === 4){
+      this.navigation = environment.ADMIN_NAVIGATION
+    }else if(this.userdata[0].roleId === 5){
+      this.navigation = environment.ADMIN_NAVIGATION
+    }
   }
-
+  public logout(){
+    this.service.logout();
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,81 @@
 
 export const environment = {
   production: false,
-  API_URL:'http://localhost:8081/'
+  API_URL:'http://localhost:8081/',
+  ADMIN_NAVIGATION :[
+    {
+      path :'/home',
+      text:'Home',
+      exact_match: true
+    },
+    {
+      path :'/home/addngo',
+      text:'Add NGO',
+      exact_match: false
+    },
+    {
+      path :'/home/ngo-request',
+      text:'NGO Requests',
+      exact_match: false
+    },
+    {
+      path :'/home/ngo',
+      text:'View NGO\'s Detail',
+      exact_match: false
+    },
+    {
+      path :'/home/donor',
+      text:'View Donator\'s Detail',
+      exact_match: false
+    },
+    {
+      path :'/home/recepient',
+      text:'View Recepient\'s Detail',
+      exact_match: false
+    }
+  ],
+  NGO_NAVIGATION :[
+    {
+      path :'',
+      text:'',
+      exact_match: true
+    }
+  ],
+  
+  DONATOR_NAVIGATION :[
+    {
+      path :'',
+      text:'',
+      exact_match: true
+    }
+  ],
+  RECEPIENT_NAVIGATION :[
+    {
+      path :'',
+      text:'',
+      exact_match: true
+    }
+  ],
+  EXECUTOR_NAVIGATION :[
+    {
+      path :'',
+      text:'',
+      exact_match: true
+    }
+  ],
+  LOGIN_NAVIGATION :[
+    {
+      path :'',
+      text:'',
+      exact_match: true
+    }
+  ],
+
+
+
+
+
+
 };
 
 /*
